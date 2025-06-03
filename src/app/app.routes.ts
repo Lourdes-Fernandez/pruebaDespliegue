dde2d7ffc37d018301fbfd60aed2579dd0de320f
equipo2000 <lulifmolina@gmail.com>
2025-06-03 10:01:02 +0100

    third commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { TestComponent } from './pages/test/test.component';
+import { Test2Component } from './pages/test2/test2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: TestComponent
+    },
+    {
+        path: 'test2',
+        component: Test2Component
+    }
+];
